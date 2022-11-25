4b7cca39ae5eaca649de9fe5f1b2b76947ff4214
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 21:44:21 +0000

    Adding tarfile member sanitization to extractall()

--- lab5/prepare_corpus.py
+++ lab5/prepare_corpus.py
@@ -32,7 +32,26 @@ def download_spam_data():
         path = os.path.join(SPAM_PATH, filename)
         urllib.request.urlretrieve(url, path)
         with tarfile.open(path) as spam_file:
-            spam_file.extractall(path=SPAM_PATH)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(spam_file, path=SPAM_PATH)
 
 
 def load_email(is_spam, filename, spam_path=SPAM_PATH):
